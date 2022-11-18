c5a9154e06632d0dbe5933c8caae26a6932bf1cb
petrostrak <pit.trak@gmail.com>
2022-11-18 13:44:39 +0200

    Debug

--- 01_hello_world/hello_world/target/debug/deps/hello_world-2fcdd7b882a8bf63.d
+++
@@ -1,5 +0,0 @@
-/Users/petrostrak/Documents/github.com/petrostrak/rust-by-example/hello_world/target/debug/deps/hello_world-2fcdd7b882a8bf63.rmeta: src/main.rs
-
-/Users/petrostrak/Documents/github.com/petrostrak/rust-by-example/hello_world/target/debug/deps/hello_world-2fcdd7b882a8bf63.d: src/main.rs
-
-src/main.rs:
--- 01_hello_world/hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/petrostrak/Documents/github.com/petrostrak/rust-by-example/hello_world/target/debug/hello_world: /Users/petrostrak/Documents/github.com/petrostrak/rust-by-example/hello_world/src/main.rs
