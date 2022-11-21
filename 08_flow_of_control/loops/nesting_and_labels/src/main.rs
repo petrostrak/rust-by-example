efb3c172a2d01c1cc2be46b5166e1531adb6a55c
petrostrak <pit.trak@gmail.com>
2022-11-21 22:00:03 +0200

    nesting and labels

---
+++ 08_flow_of_control/loops/nesting_and_labels/src/main.rs
@@ -0,0 +1,25 @@
+// It's possible to break or continue outer loops 
+// when dealing with nested loops. In these cases, 
+// the loops must be annotated with some 'label, 
+// and the label must be passed to the break/continue 
+// statement.
+
+fn main() {
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("entered the inner loop");
+
+            // This would break only the inner loop
+            // break 'inner;
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+}
