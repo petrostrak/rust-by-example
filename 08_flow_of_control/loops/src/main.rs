30c334ae805999370572c60ec9059803a49c7d10
petrostrak <pit.trak@gmail.com>
2022-11-21 21:45:49 +0200

    loop

---
+++ 08_flow_of_control/loops/src/main.rs
@@ -0,0 +1,31 @@
+// The break statement can be used to exit a loop
+// at anytime, whereas the continue statement can
+// be used to skip the rest of the iteration and
+// start a new one.
+
+fn main() {
+    let mut count = 0u32;
+
+    println!("Let's count until infinity!");
+
+    // Infinite loop
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            // Skip the rest of the iteration
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            // Exit this loop
+            break;
+        }
+    }
+}
