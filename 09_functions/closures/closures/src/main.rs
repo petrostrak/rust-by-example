44bf1f8e493e7b993bba4121fc5fa6a9f7ecc750
petrostrak <pit.trak@gmail.com>
2022-11-23 11:39:32 +0200

    closures

---
+++ 09_functions/closures/closures/src/main.rs
@@ -0,0 +1,28 @@
+// Closures are functions that can capture the enclosing environment.
+
+fn main() {
+    // Increment via closures and functions.
+    fn function(i: i32) -> i32 {
+        i + 1
+    }
+
+    // Closures are anonymous, here we are binding them to references.
+    // Annotation is identical to function annotation but is optional
+    // as are the `{}` wrapping the body. These nameless functions are
+    // assigned to appropriately named variables.
+    let closure_annotated = |i: i32| -> i32 { i + 1 };
+    let closure_inferred = |i| i + 1;
+
+    let i = 1;
+    // Call the function and closures.
+    println!("function: {}", function(i));
+    println!("closure_annotated: {}", closure_annotated(i));
+    println!("closure_inferred: {}", closure_inferred(i));
+    // Once closure's type has been inferred, it cannot be inferred again with another type
+    // println!("closure_inferred: {}", closure_inferred(2i64));
+
+    // A closure taking no arguments which returns an `i32`/
+    // The return type is inferred.
+    let one = || 1;
+    println!("closure with no arguments that returns one: {}", one());
+}
