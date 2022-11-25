22334735c0277929fbfa83e8b485e7d30d650e51
petrostrak <pit.trak@gmail.com>
2022-11-25 13:00:12 +0200

    where clauses as a bound

---
+++ 14_generics/where_clauses/src/main.rs
@@ -0,0 +1,29 @@
+// A bound can also be expressed using a `where` clause immediately before the opening `{`,
+// rather thatn at the type's first mention. Additionally, `where` clauses can apply bounds to
+// arbitrary types, rather than just to type parameters.
+
+use std::fmt::Debug;
+
+trait PrintInOption {
+    fn print_in_option(self);
+}
+
+// Because we would otherwise have to express this as `T: Debug` or
+// use another method of indirect approach, this requires a `where`
+// clause.
+impl<T> PrintInOption for T
+where
+    Option<T>: Debug,
+{
+    // We want `Option<T>: Debug` as our bound because that is what's
+    // being printed. Doing otherwise would be using the wrong bound.
+    fn print_in_option(self) {
+        println!("{:?}", Some(self));
+    }
+}
+
+fn main() {
+    let vec = vec![1, 2, 3];
+
+    vec.print_in_option();
+}
