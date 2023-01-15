76f2b8237ca4125683406ef4afbd1cf4090a230a
petrostrak <pit.trak@gmail.com>
2023-01-15 16:47:26 +0200

    Serialize custom data types

--- 22_serialization_and_deserialization/serialize_into_JSON_strings/src/main.rs
+++ 22_serialization_and_deserialization/serialize_into_JSON_strings/src/main.rs
@@ -1,5 +1,21 @@
 extern crate serde;
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Line {
+    points: Vec<Point>,
+    valid: bool,
+    length: f32,
+    desc: String,
+}
+
 fn main() {
     let x: i32 = 5;
     let xs = serde_json::to_string(&x).unwrap();
@@ -30,4 +46,28 @@ fn main() {
     let x = ((1u8, 2u16), (3.141f32, 'a'), true);
     let xs = serde_json::to_string(&x).unwrap();
     println!("nested tuple {:?} serializes into string {}", x, xs);
+
+    let point1: Point = Point { x: 1.0, y: 2.0 };
+    let point2: Point = Point { x: 3.0, y: 4.0 };
+    let point1s = serde_json::to_string(&point1).unwrap();
+    let point2s = serde_json::to_string(&point2).unwrap();
+    println!("struct Point serializes into string {}", point1s);
+    println!("struct Point serializes into string {}", point2s);
+
+    let length = ((point1.x - point2.x) * (point1.x - point2.x)
+        + (point1.y - point2.y) * (point1.y - point2.y))
+        .sqrt();
+    let valid = if length == 0.0 { false } else { true };
+    let line = Line {
+        points: vec![point1, point2],
+        valid: valid,
+        length: length,
+        desc: "a thin line".to_string(),
+    };
+    let lines = serde_json::to_string(&line).unwrap();
+    println!("struct Line serializes into string {}", lines);
+
+    let lined: Line = serde_json::from_str(&lines).unwrap();
+    assert_eq!(lined.desc, "a thin line");
+    assert_eq!(lined.points[1].x, 3.0);
 }
